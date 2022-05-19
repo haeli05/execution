53d3323591445771c506a60767dc6512a75428a6
GLaDOS III <gladosiii@GLaDOSs-MacBook-Pro.local>
2022-05-19 19:38:32 -0400

    wireframe

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,36 +1,135 @@
 import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import CssBaseline from '@mui/material/CssBaseline';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Toolbar from '@mui/material/Toolbar';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import TextField from "@mui/material/TextField";
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const mdTheme = createTheme();
 
 export default function Index() {
   return (
-    <div>
-      <AppBar style={{height:"50px", paddingBottom:"14px"}}>
-        Better Than Jupiter
-        <Link href="/">
-        Some links
-        </Link>
-        <Link href="/">
-        Connect Wallet
-        </Link>
-      </AppBar>  
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
+      <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" >
+          <Toolbar
+              sx={{
+                pr: '24px', // keep right padding when drawer closed
+              }}
+            >
+            <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                Better Than Jupiter
+              </Typography>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                Other Links
+              </Typography>
+              <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                Connect Wallet
+              </Typography>
+          </Toolbar>  
+        </AppBar>
+        <Container maxWidth="sm">
+        <Box sx={{ my: 4, paddingTop: 7 }}>
+          <Paper>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Solana Execution Engine
           </Typography>
+            {/* Card body */}
+            <Paper>
+            Pair 1:
+            <TextField>
+             
+            </TextField>
+            </Paper>
+
+            <Paper>
+            Pair 2:
+            <TextField>
+             
+            </TextField>
+            </Paper>
+            
+
           <Link href="/about" color="secondary">
             Go to the about page
           </Link>
+          </Paper>
+
+          <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            sx={{ position: 'absolute', bottom: 16, right: 16 }}
+            icon={<SpeedDialIcon />}
+          >
+            {/* {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+              />
+            ))} */}
+          </SpeedDial>
+
           <ProTip />
           <Copyright />
+          
         </Box>
       </Container>
-    </div>
+      </Box>
+      </ThemeProvider>
   );
 }
